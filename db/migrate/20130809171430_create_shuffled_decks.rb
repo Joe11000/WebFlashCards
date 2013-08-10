4648932586c08cc399b13450ea21e64aae6fa86e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-10 16:44:52 -0500

    fixed the average score by deck

--- db/migrate/20130809171430_create_shuffled_decks.rb
+++ db/migrate/20130809171430_create_shuffled_decks.rb
@@ -1,8 +1,8 @@
 class CreateShuffledDecks < ActiveRecord::Migration
   def change
     create_table :shuffled_decks do |t|
-      t.string :name
-      t.belongs_to :round
+      t.string      :name
+      t.belongs_to  :round
       t.timestamps
     end
   end
